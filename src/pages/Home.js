3747481cabf7276246143e89e960d688a992f7cd
monis-iqbal <monisfunter7@gmail.com>
2021-12-01 17:24:06 +0500

    create team

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,43 +1,71 @@
 import React, { useState } from 'react'
 import { Button } from 'antd';
 import { Link } from 'react-router-dom';
-import { Skeleton, Switch, Card, Avatar } from 'antd';
+import { Skeleton, Switch, Card, Avatar, Input } from 'antd';
 import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
-import { Modal,  } from 'antd';
+import { Modal, } from 'antd';
 import { useNavigate } from 'react-router';
-import {auth , signOut} from '../routes/Firebase'
+import { auth, signOut } from '../routes/Firebase'
+
 
 
 const Home = () => {
     let navigate = useNavigate()
-    function SignOut(){
+    function SignOut() {
         signOut(auth).then(() => {
             // Sign-out successful.
             navigate('/login')
-          }).catch((error) => {
+        }).catch((error) => {
             // An error happened.
-          });
-        }
-        //   main PAGE WORK
+        });
+    }
+    //   main PAGE WORK
 
-        const { Meta } = Card;
-    
+    const { Meta } = Card;
+
     const [loading, setLoading] = useState(false)
 
     const onChange = checked => {
         setLoading(!checked)
 
     };
+    // create team code
 
-    
-    
+
+
+
+
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+    const showModal = () => {
+        setIsModalVisible(true);
+    };
+
+    const handleOk = () => {
+        setIsModalVisible(false);
+        <Card style={{display:"block"}}/>
+    };
+
+    const handleCancel = () => {
+        setIsModalVisible(false);
+    };
+
+
+    const [showName , setShowName] = useState("");
+
+    function handleChange(event) {
+        console.log(event.target.value);
+      }
+
+
+
     return (
         <div id="home-page-div">
-            <h1 className="heading">TEAM REPORT</h1>
+            <h1 className="heading">&nbsp;&nbsp;&nbsp;TEAM REPORT</h1>
             <hr />
             <br />
             <div>
-                <h2 className="heading">Teams you own </h2>
+                <h2 className="heading">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Teams you own </h2>
 
 
                 {/* card code */}
@@ -49,11 +77,13 @@ const Home = () => {
                         <EllipsisOutlined key="ellipsis" />,
                     ]}
                     className="card-adjust"
+                    
+                    
                 >
                     <Skeleton loading={loading} avatar active>
                         <Meta
 
-                            title="Card title"
+                            title={showName}
                             description="This is the description"
                         />
                     </Skeleton>
@@ -61,12 +91,26 @@ const Home = () => {
 
                 {/* card code end */}
 
-                
+                {/* modal code */}
+
+                <Button type="primary" onClick={showModal} id="team-create-btn">
+                    CREATE TEAM </Button>
+
+
+
+                <Modal title="CREATE YOUR TEAM" visible={isModalVisible} onOk={handleOk } onCancel={handleCancel}>
+
+                    <Input placeholder="Team Name"  id="modal-input"  onChange={(ev) => { setShowName(ev.target.value)}} value={showName} />
+                    <Input placeholder="Leader Email" />
+
+                </Modal>
+
+
 
 
 
             </div>
-            <button id="team-create-btn">+</button>
+
 
 
 
